df9c9268427d7e59421624e6c52ad6ff757d1a7b
rafah mnjd <rafahmnjd@gmail.com>
2021-12-03 12:39:01 +0200

    full merge models, routers

--- models/pool.js
+++ models/pool.js
@@ -1,19 +1,4 @@
 const mysql = require('mysql');
-<<<<<<< HEAD
-
-const pool =mysql.createPool({
-    connectionLimit:400,
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:'readbooks'
-});
-<<<<<<<< HEAD:models/pool.js
-========
-
->>>>>>>> 2f8e9987bfcbd9a3367f3d23708aa52663bf5573:models/db.js
-module.exports=pool;
-=======
 const pool = mysql.createPool({
     connectionLimit: 100,
     host:"localhost",
@@ -21,6 +6,5 @@ const pool = mysql.createPool({
     password:"",
     database: "readbooks"
 });
-
 module.exports = pool;
->>>>>>> 2f8e9987bfcbd9a3367f3d23708aa52663bf5573
+
